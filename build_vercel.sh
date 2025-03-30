eb9b604f6e6d6717329e6652fd58ce3cdf3dc015
Sunakier <1722302509@qq.com>
2025-03-30 23:37:29 +0800

    chore: 删除不再使用的组件和脚本

--- build_vercel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Current branch: $VERCEL_GIT_COMMIT_REF"
-if [[ "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "dev" ]]; then 
-  echo "Branch is main or dev: $VERCEL_GIT_COMMIT_REF"
-  exit 0
-else 
-  echo "Branch is not main or dev, skipping build."
-  exit 1
-fi
